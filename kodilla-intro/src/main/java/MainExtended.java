8775b6df69a65977da1b9df69d19e5df5cd31891
Karolina Beniak <karolina.beniak4@gmail.com>
2020-08-05 23:47:41 +0200

    Main Extended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Karolina";
+        System.out.println(myName);
+        int myAge = 25;
+        System.out.println(myAge);
+        double height = 1.73;
+        System.out.println(height);
+        char sex = 'F';
+        System.out.println(sex);
+        boolean isGoodDancer = true;
+        System.out.println(isGoodDancer);
+        boolean isGoodPlayer = false;
+        System.out.println(isGoodPlayer) ;
+    }
+}
