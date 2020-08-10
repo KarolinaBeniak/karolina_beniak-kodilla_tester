213816f41b9627d227d468671251d2603550ac3b
Daniel Kierecki <dkierecki@gmail.com>
2020-08-10 19:50:26 +0200

    ekstrakcja metody

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,13 +1,17 @@
 public class LeapYear {
+
     public static void main(String[] args) {
         int year = 2020;
         System.out.println(year);
 
+        countLeap(year);
+    }
+
+    private static void countLeap(int year) {
         if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             System.out.println("Podany rok jest przestepny.");
         } else {
             System.out.println("Podany rok nie jest przestepny.");
-
         }
     }
 }
