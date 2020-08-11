cf8f832d85f31a55959ca16c5ad70464a27ba68d
Karolina Beniak <karolina.beniak4@gmail.com>
2020-08-11 20:36:46 +0200

    Second Version

---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,15 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+
+        String[] names = new String[3];
+        names[0] = "Zygfryd";
+        names[1] = "Gwidon";
+        names[2] = "Florentyna";
+
+        int numberOfElements = names.length;
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+    }
+}
