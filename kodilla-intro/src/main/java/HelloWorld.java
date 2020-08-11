ee72dd86a253e0f41b5aedabb9565472f5cbae53
Karolina Beniak <karolina.beniak4@gmail.com>
2020-08-11 21:05:27 +0200

    Second Version

--- kodilla-intro/src/main/java/HelloWorld.java
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -1,8 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Karolina");
-        System.out.println("25");
-        System.out.println("B");
+        String myName = "Karolina";
+        System.out.println(myName);
+        int myAge = 25 ;
+        System.out.println(myAge);
+        char mySurnameLetter = 'B';
+        System.out.println(mySurnameLetter);
     }
 }
