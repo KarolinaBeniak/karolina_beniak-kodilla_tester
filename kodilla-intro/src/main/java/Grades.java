5693aa147735b7f2b276d4248a2b10349ecdaa92
Karolina Beniak <karolina.beniak4@gmail.com>
2020-08-15 17:48:56 +0200

    Initial version

---
+++ kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,30 @@
+public class Grades {
+    private int[] grades;
+    private int size;
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+
+    public void add(int grades) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = grades;
+        this.size++;
+    }
+
+    public int lastGrades() {
+        if (this.size == 0) {
+            return 0;
+        }
+        return this.grades[this.size - 1];
+    }
+    public int averageGrades() {
+        if (this.size == 0) {
+            return 0;
+        }
+        return this.grades[this.size / 10];
+    }
+}
