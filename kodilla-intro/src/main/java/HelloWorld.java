ff9fd1d457b72d5d18be39796b414a6e39588335
Karolina Beniak <karolina.beniak4@gmail.com>
2020-08-24 10:14:01 +0200

    Initial version

--- kodilla-intro/src/main/java/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Karolina");
-        System.out.println(25);
-        System.out.println('B');
-    }
-}
