7e08c4b3c5590b98ad221c99e2549c4f1ad75324
Karolina Beniak <karolina.beniak4@gmail.com>
2020-08-24 22:58:30 +0200

    Initial version

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String myName = "Karolina";
+        System.out.println(myName);
+        int myAge = 25 ;
+        System.out.println(myAge);
+        char mySurnameLetter = 'B';
+        System.out.println(mySurnameLetter);
+    }
+}
+
---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,14 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2020;
+        System.out.println(year);
+        countLeap(year);
+    }
+    private static void countLeap(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println("Podany rok jest przestepny.");
+        } else {
+            System.out.println("Podany rok nie jest przestepny.");
+        }
+    }
+}
