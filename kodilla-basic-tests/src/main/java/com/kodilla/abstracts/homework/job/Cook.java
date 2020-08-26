9fc6bb3be6b90172e3a932247b46573d6900069d
Karolina Beniak <karolina.beniak4@gmail.com>
2020-08-26 18:55:02 +0200

    Initial version

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/job/Cook.java
@@ -0,0 +1,10 @@
+package com.kodilla.abstracts.homework.job;
+
+import com.kodilla.abstracts.homework.job.Job;
+
+public class Cook extends Job {
+
+    public Cook(int salary, String responsibilities) {
+        super(salary, responsibilities);
+    }
+}
