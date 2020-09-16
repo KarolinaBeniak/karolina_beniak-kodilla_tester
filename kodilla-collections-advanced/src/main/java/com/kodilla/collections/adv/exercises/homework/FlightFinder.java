ce7a0adccdb1f3aac7df155106cbe77c35212a6c
Karolina <karolina.beniak4@gmail.com>
2020-09-16 23:42:51 +0200

    myversion2

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightFinder.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightFinder.java
@@ -1,14 +1,20 @@
 package com.kodilla.collections.adv.exercises.homework;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FlightFinder {
     public List<Flight> findFlightsFrom(String departure){
+        List<Flight> listDepartures = new ArrayList<>();
+        for (Flight flight : FlightRepository.getFlightsTable()){
+            if (flight.getDeparture().equals(departure)){
+                listDepartures.add(flight);
+            }
+        }
 
     }
     public List<Flight> findFlightsTo(String arrival){
 
     }
-
 
 }
