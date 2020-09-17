9dbf42fa70b0d34bac3c9afb8727fb101515fc8c
Karolina <karolina.beniak4@gmail.com>
2020-09-17 23:01:39 +0200

    5.5. Flight list

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightFinder.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightFinder.java
@@ -4,17 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FlightFinder {
-    public List<Flight> findFlightsFrom(String departure){
+    public List<Flight> findFlightsFrom(String departure) {
         List<Flight> listDepartures = new ArrayList<>();
-        for (Flight flight : FlightRepository.getFlightsTable()){
-            if (flight.getDeparture().equals(departure)){
-                listDepartures.add(flight);
+        for (Flight startFlight : FlightRepository.getFlightsTable()) {
+            if (startFlight.getDeparture().equals(departure)) {
+                listDepartures.add(startFlight);
             }
         }
+        return listDepartures;
 
     }
-    public List<Flight> findFlightsTo(String arrival){
 
+    public List<Flight> findFlightsTo(String arrival) {
+        List<Flight> listArrivals = new ArrayList<>();
+        for (Flight endFlight : FlightRepository.getFlightsTable()) {
+            if (endFlight.getArrival().equals(arrival)) {
+                listArrivals.add(endFlight);
+            }
+        }
+        return listArrivals;
     }
 
 }
--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightRepository.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightRepository.java
@@ -1,7 +1,16 @@
 package com.kodilla.collections.adv.exercises.homework;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FlightRepository {
-    public class getFlightsTable(){
+
+    static List<Flight> getFlightsTable() {
+        List<Flight> flightList = new ArrayList<>();
+        flightList.add(new Flight("Wroclaw", "Warsaw"));
+        flightList.add(new Flight("Warsaw", "London"));
+        flightList.add(new Flight("Gdansk", "Paris"));
+        return flightList;
 
     }
 }
