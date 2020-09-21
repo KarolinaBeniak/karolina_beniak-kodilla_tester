a8d36469435bf5ee5b22fb65efb636574be8683f
Karolina <karolina.beniak4@gmail.com>
2020-09-21 14:15:15 +0200

    6.5. Order tests with exceptions

---
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
@@ -0,0 +1,29 @@
+package com.kodilla.exception.homework;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WarehouseTestSuite {
+    @Test
+    public void testIfOrderIsExist() throws OrderDoesntExistException {
+        //given
+        Warehouse warehouse = new Warehouse();
+        warehouse.addOrder(new Order("JK-55"));
+        //when
+        Order result1 = warehouse.getOrder("JK-55");
+        //then
+        assertEquals("JK-55", result1.getNumber());
+
+    }
+
+    @Test(expected = OrderDoesntExistException.class)
+    public void testIfOrderIsExist_withException() throws OrderDoesntExistException {
+        //given
+        Warehouse warehouse = new Warehouse();
+        //when
+        Order result2 = warehouse.getOrder("KB-55");
+         //then
+        assertEquals(false, result2);
+    }
+}
